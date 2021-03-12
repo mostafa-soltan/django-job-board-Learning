038e50306d312ae334cd0db5d9a23b8420826425
Mostafa Soltan <darsh.sultan8@gmail.com>
2021-03-12 07:20:14 +0300

    add media & create job image field & customiz on upload images

--- job/models.py
+++ job/models.py
@@ -13,6 +13,11 @@ JOB_TYPE = (
     ('Full Time', 'Full Time'),     
     ('Part Time', 'Part Time'),                  
 )
+
+def image_upload (instance, filename):
+    imagename, extension = filename.split('.')
+    return "jobs/%s.%s"%(instance.id,extension)
+
 class Job (models.Model):                       # Table
     title = models.CharField(max_length=100)    # Column
     # location
@@ -23,6 +28,7 @@ class Job (models.Model):                       # Table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return self.title
