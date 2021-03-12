19e743c547492c500d4ba1750a063a98a24dbfe2
Mostafa Soltan <darsh.sultan8@gmail.com>
2021-03-12 06:23:43 +0300

    Static files

--- job/views.py
+++ job/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from .models import Job
 # Create your views here.
 
-def job_list (request):
+
+def job_list(request):
     job_list = Job.objects.all()
     print(job_list)
-    context={'jobs' : job_list}
-    return render(request, 'job/job_list.html', context)
+    context = {'jobs': job_list}
+    return render(request, 'job/jobs.html', context)
+
 
 def job_detail(request, id):
-    job_detail = Job.objects.get(id = id)
-    context = {'job' : job_detail}
-    return render(request, 'job/job_detail.html', context)
-
+    job_detail = Job.objects.get(id=id)
+    context = {'job': job_detail}
+    return render(request, 'job/job_details.html', context)
